af7e7320fa79775a8dd528e49450544310f1dc65
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-07-25 00:03:15 -0500

    created questions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //0.0 the main cards are usually get things imported here
 //0. import packages; import the module from shape.js
+const { default: inquirer } = require("inquirer");
 const shape = require("./lib/shapes.js");
 const Tiangle = shape.Triangle;
 const Circle = shape.Circle;
@@ -12,7 +13,36 @@ const Square = shape.Square;
 
 const { Triangle, Circle, Square } = require("./lib/shapes.js");
 
-//1. create an array promp the user for shape, text, shape clor and text color
+//1. create an array promp the user for shape, text, shape color and text color
 //2. create the SVG
 // 2.1 gen
 // 2.9 write the file
+
+function prompUser() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter up to three characters for the logo text:",
+      name: "text",
+      validate: (input) => input.length <= 3,
+    },
+    {
+      type: "input",
+      message: "Enter the text color (keyword or hexadecimal):",
+      name: "textColor",
+    },
+    {
+      type: "list",
+      message: "Select a shape:",
+      name: "shape",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      message: "Enter the shape color (keyword or hexadecimal:",
+      name: "shapeColor",
+    },
+  ]);
+}
+
+prompUser();
